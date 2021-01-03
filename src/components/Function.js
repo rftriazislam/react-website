3f3321b3da6a7a427694d3356e3757ff7f690782
Riaz Islam <riazislam0999@gmail.com>
2021-01-03 20:16:38 +0600

    two component add

---
+++ src/components/Function.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Function(){
+    return <h1>Function way return </h1>
+}
+//arrow function ES6
+const Greet =()=><h1>Function way return Es6 arrow function </h1>
+
+export default Function
